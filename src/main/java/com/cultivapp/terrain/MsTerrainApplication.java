9f792447dd89e5f0c397451fba4f075beefa2269
Sebastián <sebastian.h.neira@outlook.com>
2024-02-22 12:19:31 -0300

    Initial Commit

---
+++ src/main/java/com/cultivapp/terrain/MsTerrainApplication.java
@@ -0,0 +1,13 @@
+package com.cultivapp.terrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTerrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTerrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cultivapp/terrain/MsTerrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cultivapp.terrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTerrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
